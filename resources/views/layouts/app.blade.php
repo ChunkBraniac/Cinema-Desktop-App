840dafb4fe6a787190a443d9de1cc61a034d4c56
Chimaobi Anthony <102151079+ChunkBraniac@users.noreply.github.com>
2024-05-10 23:28:25 +0100

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,41 +58,6 @@
         });
     </script> --}}
     <script>
-        // document.addEventListener("DOMContentLoaded", function() {
-        //     var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-
-        //     if ("IntersectionObserver" in window) {
-        //         let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-        //             entries.forEach(function(entry) {
-        //                 if (entry.isIntersecting) {
-        //                     let lazyImage = entry.target;
-        //                     lazyImage.src = lazyImage.dataset.src;
-        //                     if (lazyImage.dataset.srcset) {
-        //                         lazyImage.srcset = lazyImage.dataset.srcset;
-        //                     }
-        //                     lazyImage.classList.remove("lazy");
-        //                     lazyImageObserver.unobserve(lazyImage);
-        //                 }
-        //             });
-        //         }, {
-        //             rootMargin: "200px" // Adjust the root margin as needed for your design
-        //         });
-
-        //         lazyImages.forEach(function(lazyImage) {
-        //             lazyImageObserver.observe(lazyImage);
-        //         });
-        //     } else {
-        //         // Fallback for browsers that don't support Intersection Observer
-        //         lazyImages.forEach(function(lazyImage) {
-        //             lazyImage.src = lazyImage.dataset.src;
-        //             if (lazyImage.dataset.srcset) {
-        //                 lazyImage.srcset = lazyImage.dataset.srcset;
-        //             }
-        //             lazyImage.classList.remove("lazy");
-        //         });
-        //     }
-        // });
-
         // Create an Intersection Observer instance
         let observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
