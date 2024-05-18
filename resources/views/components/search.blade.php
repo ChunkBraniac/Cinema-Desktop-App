0662057f0b2dafe0edbc8bb0f6503e4508edaa9f
Chimaobi Anthony <102151079+ChunkBraniac@users.noreply.github.com>
2024-05-18 22:01:45 +0100

    added some new features

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -32,15 +32,15 @@
                 @unless (count($paginatedResults) == 0)
                     @foreach ($paginatedResults as $search)
                         <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 mt-3">
-                            <a href="{{ url('media/' . $search->originalTitleText . '/' . $search->titleType) }}"><img
-                                    data-src="{{ asset($search->imageUrl) }}" alt="" class="img-fluid blurry-image lazy"
-                                    style="width: 100%; aspect-ratio: 3/5;" loading="lazy"></a>
+                            <a href="{{ url('media/' . $search->originalTitleText . '/' . $search->titleType) }}" class="text-decoration-none text-dark"><img
+                                    data-src="{{ asset($search->imageUrl) }}" alt="{{ str_replace('-', ' ', $search->originalTitleText) . ' ' . '(' . $search->releaseYear . ')' }}" class="img-fluid blurry-image lazy"
+                                    style="width: 100%; aspect-ratio: 3/5; background: rgba(0, 0, 0, 0.493);" loading="lazy"></a>
                             <a href="{{ url('media/' . $search->originalTitleText . '/' . $search->titleType) }}"
                                 class="text-decoration-none text-dark" data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                data-bs-title="{{ $search->originalTitleText . ' ' . '(' . $search->releaseYear . ')' }}">
+                                data-bs-title="{{ str_replace('-', ' ', $search->originalTitleText) . ' ' . '(' . $search->releaseYear . ')' }}">
 
                                 <h6 class="mt-1 text-truncate" style="font-family: 'Robot', sans-serif; font-weight: 500">
-                                    {{ $search->originalTitleText . ' ' . '(' . $search->releaseYear . ')' }}</h6>
+                                    {{ str_replace('-', ' ', $search->originalTitleText) . ' ' . '(' . $search->releaseYear . ')' }}</h6>
                             </a>
 
                             @if ($search->genres == '0')
@@ -59,6 +59,8 @@
                         </div>
                     @endforeach
                 @endunless
+
+                
             @else
                 <p class="alert alert-danger">No results found for '{{ $searchWord }}'</p>
 
@@ -73,6 +75,10 @@
 
             <div class="mt-3">
                 {{ $paginatedResults->appends(request()->query())->onEachSide(1)->links('vendor.pagination.bootstrap-4') }}
+
+                @if (count($paginatedResults) == 0)
+                    {{ abort(404) }}
+                @endif
             </div>
         </div>
 
--- resources/views/components/show-more.blade.php
+++ resources/views/components/show-more.blade.php
@@ -26,7 +26,6 @@
                     class="fa fa-arrow-right text-muted" style="font-size: 13px" aria-hidden="true"></i> <span
                     style="margin-left: 5px; font-size: 14px" class="text-muted">Series</span></h6>
         </div>
-
     @elseif ($cartegory == 'movies')
         <div class="container">
             <h4 style="float: left">Movies</h4>
@@ -42,60 +41,25 @@
     <hr>
     <div class="container mt-5">
 
-        {{-- Movies Pane --}}
-        @if ($cartegory == 'movies')
-            <div class="row mb-4">
-                @unless (count($more_Movies) == 0)
-                    @foreach ($more_Movies as $action)
-                        <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 mt-3">
-                            <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}"><img
-                                data-src="{{ asset($action->imageUrl) }}" alt="" class="img-fluid blurry-image lazy"
-                                style="width: 100%; aspect-ratio: 3/5; background: rgba(0, 0, 0, 0.493)" loading="lazy"></a>
-                            <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}"
-                                class="text-decoration-none text-dark">
-
-                                <h6 class="mt-1 text-truncate" style="font-family: 'Ubuntu sans', sans-serif; font-weight: 500" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="{{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}">
-                                    {{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')'  }}</h6>
-                            </a>
-                            @if ($action->genres == 0)
-                                <h6 class="text-truncate"
-                                    style="font-size: 14px; font-family: 'Roboto', sans-serif; font-weight: 400">
-                                    {{ $action->genres = 'N/A' }}</h6>
-                            @elseif ($action->genres == '')
-                                <h6 class="text-truncate"
-                                    style="font-size: 14px; font-family: 'Roboto', sans-serif; font-weight: 400">
-                                    {{ $action->genres = 'N/A' }}</h6>
-                            @else
-                                <h6 class="text-truncate"
-                                    style="font-size: 14px; font-family: 'Roboto', sans-serif; font-weight: 400">
-                                    {{ $action->genres }}</h6>
-                            @endif
-                        </div>
-                    @endforeach
-                @endunless
-            </div>
-
-            {{ $more_Movies->appends(request()->query())->onEachSide(1)->links('vendor.pagination.bootstrap-4') }}
-
-            @if (count($more_Movies) == 0)
-                {{ abort(404) }}
-            @endif
-
 
         {{-- Series Pane --}}
-        @elseif ($cartegory == 'series')
+        @if ($cartegory == 'series')
             <div class="row mb-4">
                 @unless (count($more_Series) == 0)
                     @foreach ($more_Series as $action)
                         <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 mt-3">
-                            <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}"><img
-                                data-src="{{ asset($action->imageUrl) }}" alt="" class="img-fluid blurry-image lazy"
-                                style="width: 100%; aspect-ratio: 3/5; background: rgba(0, 0, 0, 0.493);" loading="lazy"></a>
+                            <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}" class="text-decoration-none text-dark"><img
+                                    data-src="{{ asset($action->imageUrl) }}" alt="{{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}" class="img-fluid blurry-image lazy"
+                                    style="width: 100%; aspect-ratio: 3/5; background: rgba(0, 0, 0, 0.493);"
+                                    loading="lazy"></a>
                             <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}"
                                 class="text-decoration-none text-dark">
 
-                                <h6 class="mt-1 text-truncate" style="font-family: 'Ubuntu sans', sans-serif; font-weight: 500" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="{{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}">
-                                    {{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}</h6>
+                                <h6 class="mt-1 text-truncate" style="font-family: 'Ubuntu sans', sans-serif; font-weight: 500"
+                                    data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                    data-bs-title="{{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}">
+                                    {{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}
+                                </h6>
                             </a>
                             @if ($action->genres == 0)
                                 <h6 class="text-truncate"
@@ -121,7 +85,51 @@
                 {{ abort(404) }}
             @endif
 
-        @endif
+            {{-- Movies Pane --}}
+            @elseif ($cartegory == 'movies')
+                <div class="row mb-4">
+                    @unless (count($more_Movies) == 0)
+                        @foreach ($more_Movies as $action)
+                            <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 mt-3">
+                                <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}" class="text-decoration-none text-dark"><img
+                                        data-src="{{ asset($action->imageUrl) }}" alt="{{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}"
+                                        class="img-fluid blurry-image lazy"
+                                        style="width: 100%; aspect-ratio: 3/5; background: rgba(0, 0, 0, 0.493)"
+                                        loading="lazy"></a>
+                                <a href="{{ url('media/' . $action->originalTitleText . '/' . $action->titleType) }}"
+                                    class="text-decoration-none text-dark">
+
+                                    <h6 class="mt-1 text-truncate"
+                                        style="font-family: 'Ubuntu sans', sans-serif; font-weight: 500"
+                                        data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                        data-bs-title="{{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}">
+                                        {{ str_replace('-', ' ', $action->originalTitleText) . ' ' . '(' . $action->releaseYear . ')' }}
+                                    </h6>
+                                </a>
+                                @if ($action->genres == 0)
+                                    <h6 class="text-truncate"
+                                        style="font-size: 14px; font-family: 'Roboto', sans-serif; font-weight: 400">
+                                        {{ $action->genres = 'N/A' }}</h6>
+                                @elseif ($action->genres == '')
+                                    <h6 class="text-truncate"
+                                        style="font-size: 14px; font-family: 'Roboto', sans-serif; font-weight: 400">
+                                        {{ $action->genres = 'N/A' }}</h6>
+                                @else
+                                    <h6 class="text-truncate"
+                                        style="font-size: 14px; font-family: 'Roboto', sans-serif; font-weight: 400">
+                                        {{ $action->genres }}</h6>
+                                @endif
+                            </div>
+                        @endforeach
+                    @endunless
+                </div>
+
+                {{ $more_Movies->appends(request()->query())->onEachSide(1)->links('vendor.pagination.bootstrap-4') }}
+
+                @if (count($more_Movies) == 0)
+                    {{ abort(404) }}
+                @endif
+            @endif
 
     </div>
 @endsection
